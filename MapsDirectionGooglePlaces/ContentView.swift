e958b0aeb73bc45b2de0d9f7267708d2bd346d3d
Chris Sanders <astericky@gmail.com>
2020-05-18 21:33:49 -0700

    Initial Commit

---
+++ MapsDirectionGooglePlaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapsDirectionGooglePlaces
+//
+//  Created by Chris Sanders on 5/18/20.
+//  Copyright © 2020 Chris Sanders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
